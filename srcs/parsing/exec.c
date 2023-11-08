8abdca565b33524d5658185ec74a7c6268726d2e
Артем Апенко <lbapart@MacBook-Pro.local>
2023-11-08 08:56:06 +0100

    and exec also xD

--- srcs/parsing/exec.c
+++ srcs/parsing/exec.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   exec.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ppfiel <ppfiel@student.42.fr>              +#+  +:+       +#+        */
+/*   By: lbapart <lbapart@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/02 15:45:19 by lbapart           #+#    #+#             */
-/*   Updated: 2023/11/07 16:05:49 by ppfiel           ###   ########.fr       */
+/*   Updated: 2023/11/08 08:50:42 by lbapart          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,13 +28,6 @@ int	exec_builtin(t_smplcmd *smplcmd, t_shell *shell)
 		return (execute_env(*smplcmd, *shell));
 	if (smplcmd->builtin == EXIT)
 		return (execute_exit(*smplcmd));
-	return (1);
-}
-
-int	exec_heredoc(t_redirection *redir)
-{
-	(void)redir;
-	printf("heredoc\n");
 	return (1);
 }
 
@@ -130,6 +123,14 @@ void	exec_commands(char *cmd, t_shell *shell)
 				else if (redirections->type == 2)
 				{
 					//TODO: Heredoc: Call heredoc function and then redirect STDIN to filedescriptor
+					exec_heredoc(redirections, 1);
+					file_descriptor = open(redirections->file, O_RDONLY, 0000644);
+					if (file_descriptor == -1)
+						return (perror(redirections->file));
+					if (dup2(file_descriptor, STDIN_FILENO) == -1)
+						return (perror("dup2"), close(file_descriptor), exit(EXIT_FAILURE)); // free also
+					if (close(file_descriptor) == -1)
+						return (perror("close"), exit(EXIT_FAILURE));
 				}
 				else if (redirections->type == 3)
 				{
@@ -222,6 +223,14 @@ void	exec_commands(char *cmd, t_shell *shell)
 				else if (redirections->type == 2)
 				{
 					//TODO: Heredoc: Call heredoc function and then redirect STDIN to filedescriptor
+					exec_heredoc(redirections, temp->pid);
+					file_descriptor = open(redirections->file, O_RDONLY, 0000644);
+					if (file_descriptor == -1)
+						return (perror(redirections->file));
+					if (dup2(file_descriptor, STDIN_FILENO) == -1)
+						return (perror("dup2"), close(file_descriptor), exit(EXIT_FAILURE)); // free also
+					if (close(file_descriptor) == -1)
+						return (perror("close"), exit(EXIT_FAILURE));
 				}
 				else if (redirections->type == 3)
 				{
@@ -295,75 +304,63 @@ void	exec_commands(char *cmd, t_shell *shell)
 
 
 // also need to handle ctrl + c and ctrl + d here
-int	redirect_input_output(t_redirection *redir)
-{
-	int fd;
+// int	redirect_input_output(t_redirection *redir)
+// {
+// 	int fd;
 
-	if (redir->type == 1)
-	{
-		fd = open(redir->file, O_RDONLY);
-		if (fd == -1)
-			return (perror(redir->file), 1);
-		if (dup2(fd, STDIN_FILENO) == -1)
-			return (perror("dup2"), close(fd), 1);
-		if (close(fd) == -1)
-			return (perror("close")), 1;
-	}
-	else if (redir->type == 2)
-	{
-		return (exec_heredoc(redir));
-	}
-	else if (redir->type == 3)
-	{
-		fd = open(redir->file, O_WRONLY | O_CREAT | O_TRUNC, 0644);
-		if (fd == -1)
-			return (perror(redir->file), 1);
-		if (dup2(fd, STDOUT_FILENO) == -1)
-			return (perror("dup2"), close(fd), 1);
-		if (close(fd) == -1)
-			return (perror("close"), 1);
-	}
-	else if (redir->type == 4)
-	{
-		fd = open(redir->file, O_WRONLY | O_CREAT | O_APPEND, 0644);
-		if (fd == -1)
-			return (perror(redir->file), 1);
-		if (dup2(fd, STDOUT_FILENO) == -1)
-			return (perror("dup2"), close(fd), 1);
-		if (close(fd) == -1)
-			return (perror("close"), 1);
-	}
-	return (1);
-}
+// 	if (redir->type == 1)
+// 	{
+// 		fd = open(redir->file, O_RDONLY);
+// 		if (fd == -1)
+// 			return (perror(redir->file), 1);
+// 		if (dup2(fd, STDIN_FILENO) == -1)
+// 			return (perror("dup2"), close(fd), 1);
+// 		if (close(fd) == -1)
+// 			return (perror("close")), 1;
+// 	}
+// 	else if (redir->type == 2)
+// 	{
+// 		return (exec_heredoc(redir));
+// 	}
+// 	else if (redir->type == 3)
+// 	{
+// 		fd = open(redir->file, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+// 		if (fd == -1)
+// 			return (perror(redir->file), 1);
+// 		if (dup2(fd, STDOUT_FILENO) == -1)
+// 			return (perror("dup2"), close(fd), 1);
+// 		if (close(fd) == -1)
+// 			return (perror("close"), 1);
+// 	}
+// 	else if (redir->type == 4)
+// 	{
+// 		fd = open(redir->file, O_WRONLY | O_CREAT | O_APPEND, 0644);
+// 		if (fd == -1)
+// 			return (perror(redir->file), 1);
+// 		if (dup2(fd, STDOUT_FILENO) == -1)
+// 			return (perror("dup2"), close(fd), 1);
+// 		if (close(fd) == -1)
+// 			return (perror("close"), 1);
+// 	}
+// 	return (1);
+// }
 
 int	exec_simple_command(t_smplcmd *smplcmd, t_shell *shell)
 {
-	t_redirection	*temp;
 	char	*path;
 	char	**args;
+	char	**env_path;
 
 	(void)shell; // temp
-	temp = smplcmd->redir;
-	path = NULL;
 	args = smplcmd->args;
-	while (temp)
-	{
-		redirect_input_output(temp);
-		temp = temp->next;
-	}
-	if (!smplcmd->path && smplcmd->args)
-	{
-		path = smplcmd->args[0];
-		args = &smplcmd->args[1];
-	}
-	else if (smplcmd->path)
-	{
-		path = smplcmd->path;
-		args = smplcmd->args;
-	}
+	path = smplcmd->path;
+	// env_path = get_env_path(shell->env);
+	// if (!env_path)
+	// 	env_path = get_env_path(shell->exported_vars);
+	// path = get_path(env_path, smplcmd->path);
 	// need to handle exit code here
 	if (path && execve(path, args, NULL) == -1) // need to set env 
-			return (perror("execve"), 1);
+		return (free(path), perror("execve"), 1);
 	else
-		return (1);
+		return (free(path), 1);
 }
